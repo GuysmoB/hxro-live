f2d46a9ef1a67b5e73d70b895aa5652773dc985d
rbrauge <rbrauge@.asi.fr>
2021-07-14 22:31:22 +0200

    init node

---
+++ src/app.ts
@@ -0,0 +1,446 @@
+// https://www.digitalocean.com/community/tutorials/setting-up-a-node-project-with-typescript
+// https://github.com/nikvdp/pidcrypt/issues/5#issuecomment-511383690
+// https://github.com/Microsoft/TypeScript/issues/17645#issuecomment-320556012
+
+process.env.NTBA_FIX_319 = "1"; // disable Telegram error
+import { IndicatorsService } from "./services/indicators.service";
+import { CandleAbstract } from "./abstract/candleAbstract";
+import { StrategiesService } from "./services/strategies-service";
+import { UtilsService } from "./services/utils-service";
+import { Config } from "./config";
+import firebase from "firebase";
+import TelegramBot from "node-telegram-bot-api";
+
+// VARIABLES
+let allData = [];
+let winTrades = [];
+let loseTrades = [];
+let allTrades = [];
+let telegramBot: any;
+const toDataBase = false;
+
+class App extends CandleAbstract {
+  constructor(
+    private utils: UtilsService,
+    private stratService: StrategiesService,
+    private config: Config,
+    private indicators: IndicatorsService
+  ) {
+    super();
+    /* firebase.initializeApp(config.firebaseConfig);
+    telegramBot = new TelegramBot(config.token, { polling: false }); */
+
+    /* allData = this.utils.dataArrayBuilder(items, allData, timeFrameArray);
+    this.init(); */
+  }
+
+  /**
+   * Point d'entrée.
+   */
+  async init(): Promise<void> {
+    try {
+      /* await this.utils.login(ig);
+      ig.connectToLightstreamer();
+      ig.subscribeToLightstreamer("MERGE", items, ["BID", "OFFER"], 3);
+      ig.lsEmitter.on("update", (streamData: any) => {
+        const minuteTimestamp = Math.trunc(Date.now() / 60000);
+        const ticker = this.utils.getTicker(streamData[1]);
+        const price = this.utils.round(
+          parseFloat(streamData[2]) +
+            (parseFloat(streamData[3]) - parseFloat(streamData[2])) / 2,
+          5
+        );
+
+        for (const timeFrame of timeFrameArray) {
+          const tickerTf = ticker + "_" + timeFrame + "MINUTE";
+
+          if (
+            minuteTimestamp % timeFrame === 0 &&
+            !allData[tickerTf].timeProcessed.find(
+              (element: any) => element === minuteTimestamp
+            )
+          ) {
+            allData[tickerTf].timeProcessed.push(minuteTimestamp);
+
+            if (allData[tickerTf].ohlc_tmp) {
+              const lastCandle = allData[tickerTf].ohlc_tmp;
+              lastCandle.close = price;
+              allData[tickerTf].ohlc.push(lastCandle);
+              this.findSetupOnClosedCandles(tickerTf);
+            }
+            allData[tickerTf].ohlc_tmp = {
+              date: streamData[0],
+              open: price,
+              high: price,
+              low: price,
+            };
+          }
+
+          if (allData[tickerTf].ohlc_tmp) {
+            let currentCandlestick = allData[tickerTf].ohlc_tmp;
+            if (price > currentCandlestick.high) {
+              currentCandlestick.high = price;
+            }
+            if (price < currentCandlestick.low) {
+              currentCandlestick.low = price;
+            }
+          }
+          this.entryExit(price, tickerTf, streamData[0]);
+        }
+      }); */
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  /**
+   * Execution de la stratégie principale.
+   */
+  entryExit(price: number, tickerTf: string, date: Date) {
+    let rr: number;
+    const rrTarget = 2;
+    const data = allData[tickerTf].ohlc;
+    const inLong = this.getDirection_Long(tickerTf);
+    const inShort = this.getDirection_Short(tickerTf);
+
+    try {
+      if (inLong) {
+        rr = this.stratService.getFixedTakeProfitAndStopLoss(
+          "LONG",
+          this.getTickerTfData(tickerTf),
+          price
+        );
+        this.updateResults("LONG", rr, tickerTf);
+      } else {
+        const res = this.stratService.trigger_EngulfingRetested_Long(
+          this.getSnapshot_Long(tickerTf),
+          price
+        );
+        if (res) {
+          const date = this.utils.getDate();
+          this.setDirection_Long(tickerTf, true);
+          this.setSnapshotCanceled_Long(tickerTf, true);
+          this.setEntryTime_Long(tickerTf, date);
+          this.setEntryPrice_Long(
+            tickerTf,
+            this.utils.round(res.entryPrice, 5)
+          );
+          this.setStopLoss_Long(tickerTf, this.utils.round(res.stopLoss, 5));
+          this.setTakeProfit_Long(
+            tickerTf,
+            this.utils.round(
+              res.entryPrice + (res.entryPrice - res.stopLoss) * rrTarget,
+              5
+            )
+          );
+
+          if (this.logEnable) {
+            console.log("--------");
+            console.log(
+              "Bullish Engulfing",
+              data[this.getSnapshot_Long(tickerTf).time].date
+            );
+            console.log("Long", tickerTf, date);
+            console.log("entryPrice", this.getEntryPrice_Long(tickerTf));
+            console.log("stopLoss", this.getStopLoss_Long(tickerTf));
+            console.log("takeProfit", this.getTakeProfit_Long(tickerTf));
+          }
+        }
+      }
+
+      if (inShort) {
+        rr = this.stratService.getFixedTakeProfitAndStopLoss(
+          "SHORT",
+          this.getTickerTfData(tickerTf),
+          price
+        );
+        this.updateResults("SHORT", rr, tickerTf);
+      } else {
+        const res = this.stratService.trigger_EngulfingRetested_Short(
+          this.getSnapshot_Short(tickerTf),
+          price
+        );
+        if (res) {
+          const date = this.utils.getDate();
+          this.setDirection_Short(tickerTf, true);
+          this.setSnapshotCanceled_Short(tickerTf, true);
+          this.setEntryTime_Short(tickerTf, date);
+          this.setEntryPrice_Short(
+            tickerTf,
+            this.utils.round(res.entryPrice, 5)
+          );
+          this.setStopLoss_Short(tickerTf, this.utils.round(res.stopLoss, 5));
+          this.setTakeProfit_Short(
+            tickerTf,
+            this.utils.round(
+              res.entryPrice + (res.entryPrice - res.stopLoss) * rrTarget,
+              5
+            )
+          );
+
+          if (this.logEnable) {
+            console.log("--------");
+            console.log(
+              "Bearish Engulfing",
+              data[this.getSnapshot_Short(tickerTf).time].date
+            );
+            console.log("Short", tickerTf, date);
+            console.log("entryPrice", this.getEntryPrice_Short(tickerTf));
+            console.log("stopLoss", this.getStopLoss_Short(tickerTf));
+            console.log("takeProfit", this.getTakeProfit_Short(tickerTf));
+          }
+        }
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  /**
+   * Update trades's state, global R:R and log.
+   */
+  updateResults(direction: string, rr: number, tickerTf: any) {
+    try {
+      if (rr !== undefined) {
+        allTrades.push(rr);
+        if (rr >= 0) {
+          winTrades.push(rr);
+        } else if (rr < 0) {
+          loseTrades.push(rr);
+        }
+
+        if (toDataBase) {
+          this.utils.insertTrade(
+            this.getTickerTfData(tickerTf),
+            tickerTf,
+            winTrades,
+            loseTrades,
+            allTrades
+          );
+        }
+
+        if (direction === "LONG") {
+          this.setDirection_Long(tickerTf, false);
+        } else if (direction === "SHORT") {
+          this.setDirection_Short(tickerTf, false);
+        } else {
+          console.error("Long or short ?");
+        }
+
+        console.log("-------------------------");
+        console.log("---- UPDATED RESULTS ----");
+        console.log("-------------------------");
+        console.log("Last R:R", rr);
+        console.log(direction, tickerTf, this.utils.getDate());
+        console.log(
+          "Trades : Gagnes / Perdus / Total",
+          winTrades.length,
+          loseTrades.length,
+          winTrades.length + loseTrades.length
+        );
+        console.log(
+          "Total R:R",
+          this.utils.round(
+            loseTrades.reduce((a, b) => a + b, 0) +
+              winTrades.reduce((a, b) => a + b, 0),
+            2
+          )
+        );
+        console.log(
+          "Avg R:R",
+          this.utils.round(
+            allTrades.reduce((a, b) => a + b, 0) / allTrades.length,
+            2
+          )
+        );
+        console.log(
+          "Winrate " +
+            this.utils.round(
+              (winTrades.length / (loseTrades.length + winTrades.length)) * 100,
+              2
+            ) +
+            "%"
+        );
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  /**
+   * Recherche de setup sur les candles closes et les sauvegarde dans AllData
+   */
+  findSetupOnClosedCandles(tickerTf: string) {
+    try {
+      const data = allData[tickerTf].ohlc;
+      const atr = this.indicators.atr(data, 10);
+      const inLong = this.getDirection_Long(tickerTf);
+      const inShort = this.getDirection_Short(tickerTf);
+
+      if (!inLong) {
+        const isLongSetup = this.stratService.strategy_EngulfingRetested_Long(
+          data,
+          atr
+        );
+        if (isLongSetup) {
+          this.setSnapshot_Long(tickerTf, isLongSetup);
+        }
+      }
+      if (!inShort) {
+        const isShortSetup = this.stratService.strategy_EngulfingRetested_Short(
+          data,
+          atr
+        );
+        if (isShortSetup) {
+          this.setSnapshot_Short(tickerTf, isShortSetup);
+        }
+      }
+
+      const isLiquidityShort = this.stratService.checkLiquidity_Short(
+        data,
+        atr
+      );
+      const isLiquidityLong = this.stratService.checkLiquidity_Long(data, atr);
+      if (isLiquidityLong) {
+        //console.log(tickerTf + ' | ' + this.utils.getDate() + ' | Bullish liquidity found !');
+        this.setLiquidity_Long(tickerTf, isLiquidityLong);
+      }
+      if (isLiquidityShort) {
+        //console.log(tickerTf + ' | ' + this.utils.getDate() + ' | Bearish liquidity found !');
+        this.setLiquidity_Short(tickerTf, isLiquidityShort);
+      }
+
+      const isLiquidityShortSetup =
+        this.stratService.strategy_LiquidityBreakout_Short(
+          data,
+          this.getLiquidity_Short(tickerTf)
+        );
+      const isLiquidityLongSetup =
+        this.stratService.strategy_LiquidityBreakout_Long(
+          data,
+          this.getLiquidity_Long(tickerTf)
+        );
+      if (isLiquidityLongSetup) {
+        this.setLiquidity_Long(tickerTf, undefined);
+        this.utils.sendTelegramMsg(
+          telegramBot,
+          this.config.chatId,
+          tickerTf + " | Bullish liquidity setup"
+        );
+      }
+      if (isLiquidityShortSetup) {
+        this.setLiquidity_Short(tickerTf, undefined);
+        this.utils.sendTelegramMsg(
+          telegramBot,
+          this.config.chatId,
+          tickerTf + " | Bearish liquidity setup"
+        );
+      }
+    } catch (error) {
+      console.error(error);
+      this.utils.stopProcess();
+    }
+  }
+
+  /**
+   * GETTER / SETTER
+   */
+  getLiquidity_Long(tickerTf: any) {
+    return allData[tickerTf].liquidity_Long;
+  }
+  getTickerTfData(tickerTf: any) {
+    return allData[tickerTf];
+  }
+  getDirection_Long(tickerTf: any) {
+    return allData[tickerTf].inLong;
+  }
+  getEntryPrice_Long(tickerTf: any) {
+    return allData[tickerTf].entryPrice_Long;
+  }
+  getStopLoss_Long(tickerTf: any) {
+    return allData[tickerTf].initialStopLoss_Long;
+  }
+  getTakeProfit_Long(tickerTf: any) {
+    return allData[tickerTf].takeProfit_Long;
+  }
+  getSnapshot_Long(tickerTf: any) {
+    return allData[tickerTf].snapshot_Long;
+  }
+
+  setLiquidity_Long(tickerTf: any, value: any) {
+    allData[tickerTf].liquidity_Long = value;
+  }
+  setEntryTime_Long(tickerTf: any, value: any) {
+    allData[tickerTf].entryTime_Long = value;
+  }
+  setSnapshot_Long(tickerTf: any, value: any) {
+    allData[tickerTf].snapshot_Long = value;
+  }
+  setSnapshotCanceled_Long(tickerTf: any, value: boolean) {
+    allData[tickerTf].snapshot_Long.canceled = value;
+  }
+  setDirection_Long(tickerTf: any, value: boolean) {
+    allData[tickerTf].inLong = value;
+  }
+  setEntryPrice_Long(tickerTf: any, value: number) {
+    allData[tickerTf].entryPrice_Long = value;
+  }
+  setStopLoss_Long(tickerTf: any, value: number) {
+    allData[tickerTf].initialStopLoss_Long = value;
+  }
+  setTakeProfit_Long(tickerTf: any, value: number) {
+    allData[tickerTf].takeProfit_Long = value;
+  }
+
+  getLiquidity_Short(tickerTf: any) {
+    return allData[tickerTf].liquidity_Short;
+  }
+  getDirection_Short(tickerTf: any) {
+    return allData[tickerTf].inShort;
+  }
+  getEntryPrice_Short(tickerTf: any) {
+    return allData[tickerTf].entryPrice_Short;
+  }
+  getStopLoss_Short(tickerTf: any) {
+    return allData[tickerTf].initialStopLoss_Short;
+  }
+  getTakeProfit_Short(tickerTf: any) {
+    return allData[tickerTf].takeProfit_Short;
+  }
+  getSnapshot_Short(tickerTf: any) {
+    return allData[tickerTf].snapshot_Short;
+  }
+
+  setLiquidity_Short(tickerTf: any, value: any) {
+    allData[tickerTf].liquidity_Short = value;
+  }
+  setEntryTime_Short(tickerTf: any, value: any) {
+    allData[tickerTf].entryTime_Short = value;
+  }
+  setSnapshot_Short(tickerTf: any, value: any) {
+    allData[tickerTf].snapshot_Short = value;
+  }
+  setSnapshotCanceled_Short(tickerTf: any, value: boolean) {
+    allData[tickerTf].snapshot_Short.canceled = value;
+  }
+  setDirection_Short(tickerTf: any, value: boolean) {
+    allData[tickerTf].inShort = value;
+  }
+  setEntryPrice_Short(tickerTf: any, value: number) {
+    allData[tickerTf].entryPrice_Short = value;
+  }
+  setStopLoss_Short(tickerTf: any, value: number) {
+    allData[tickerTf].initialStopLoss_Short = value;
+  }
+  setTakeProfit_Short(tickerTf: any, value: number) {
+    allData[tickerTf].takeProfit_Short = value;
+  }
+}
+
+const utilsService = new UtilsService();
+new App(
+  utilsService,
+  new StrategiesService(utilsService),
+  new Config(),
+  new IndicatorsService(utilsService)
+);
---
+++ src/services/utils-service.ts
@@ -0,0 +1,324 @@
+import { promisify } from 'util';
+import firebase from 'firebase';
+import fs from 'fs';
+import ig from 'node-ig-api';
+import { error } from 'console';
+
+
+export class UtilsService {
+  constructor() { }
+
+
+  /**
+   * Parse et push les donnees CSV.
+   */
+  async getDataFromFile(): Promise<any> {
+    const result = [];
+    const content = await promisify(fs.readFile)('src\\assets\\EURUSD60.csv', 'UTF-8');
+    const csvToRowArray = content.split('\r\n');
+    for (let index = 1; index < csvToRowArray.length - 1; index++) {
+      const element = csvToRowArray[index].split('\t'); // d, o, h, l, c, v
+      result.push({
+        date: element[0],
+        open: parseFloat(element[1]),
+        high: parseFloat(element[2]),
+        low: parseFloat(element[3]),
+        close: parseFloat(element[4]),
+        volume: parseFloat(element[5]),
+      });
+    }
+    return result;
+  }
+
+
+  /**
+   * Parse les données depuis IG.
+   */
+  async parseData(ticker: string, resolution: string, max: number) {
+    const result = [];
+    const res = await ig.get('/prices/CS.D.' + ticker + '.CFD.IP?resolution=' + resolution + '&max=' + max + '&pageSize=0', 3);
+    for (let i = 0; i < res.body.prices.length; i++) {
+      result.push({
+        open: res.body.prices[i].openPrice.bid,
+        close: res.body.prices[i].closePrice.bid,
+        high: res.body.prices[i].highPrice.bid,
+        low: res.body.prices[i].lowPrice.bid
+      });
+    }
+    return result;
+  }
+
+
+  /**
+   * Permet de retourner le R:R
+   */
+  getRiskReward(entryPrice: number, initialStopLoss: number, closedPrice: number): number {
+    const result = this.round(Math.abs(closedPrice - entryPrice) / Math.abs(entryPrice - initialStopLoss), 2);
+    if (isNaN(result)) {
+      return -1;
+    } else {
+      return result;
+    }
+  }
+
+
+  /**
+   * Retourne la valeur maximale en fonction de la source et de lookback
+   */
+  highest(data: any, index: number, source: string, lookback: number): number {
+    let max: number;
+
+    for (let k = 0; k < lookback; k++) {
+      if (k === 0) {
+        max = data[index - k][source];
+      }
+
+      if (data[index - k][source] > max) {
+        max = data[index - k][source];
+      }
+    }
+    return max;
+  }
+
+
+  /**
+   * Retourne la valeur minimale en fonction de la source et de lookback
+   */
+  lowest(data: any, index: number, source: string, lookback: number): number {
+    let min: number;
+
+    for (let k = 0; k < lookback; k++) {
+      if (k === 0) {
+        min = data[index - k][source];
+      }
+
+      if (data[index - k][source] < min) {
+        min = data[index - k][source];
+      }
+    }
+    return min;
+  }
+
+
+  /**
+   * Arrondi un nombre avec une certaine précision.
+   */
+  round(value: number, precision: number): number {
+    const multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+  }
+
+
+  /**
+   * Retourne l'équivalent HeikenAshi
+   */
+  setHeikenAshiData(source: any): any {
+    const result = [];
+
+    for (let j = 0; j < source.length; j++) {
+      if (j === 0) {
+        const _close = this.round((source[j].open + source[j].high + source[j].low + source[j].close) / 4, 5);
+        const _open = this.round((source[j].open + source[j].close) / 2, 5);
+        result.push({
+          close: _close,
+          open: _open,
+          low: source[j].low,
+          high: source[j].high,
+          bull: _close > _open,
+          bear: _close < _open,
+        });
+      } else {
+        const haCloseVar = (source[j].open + source[j].high + source[j].low + source[j].close) / 4;
+        const haOpenVar = (result[result.length - 1].open + result[result.length - 1].close) / 2;
+        result.push({
+          close: this.round(haCloseVar, 5),
+          open: this.round(haOpenVar, 5),
+          low: this.round(Math.min(source[j].low, Math.max(haOpenVar, haCloseVar)), 5),
+          high: this.round(Math.max(source[j].high, Math.max(haOpenVar, haCloseVar)), 5),
+          bull: haCloseVar > haOpenVar,
+          bear: haCloseVar < haOpenVar,
+        });
+      }
+    }
+    return result;
+  }
+
+
+  /**
+   * Retourne une moving average en fonction de la période.
+   */
+  sma(data: any, index: number, periode: number): number {
+    const result = [];
+    const dataStart = index - periode;
+
+    if (dataStart > 0) {
+      for (let i = dataStart; i < index; i++) {
+        result.push(data[i].close);
+      }
+      return this.round(result.reduce((a, b) => a + b, 0) / result.length, 5);
+    } else {
+      return 0;
+    }
+  }
+
+
+  dataArrayBuilder(epic: any, allData: any, timeFrame: any) {
+    for (let i = 0; i < epic.length; i++) {
+      for (let j = 0; j < timeFrame.length; j++) {
+        const tickerTf = this.getTicker(epic[i]) + '_' + timeFrame[j] + 'MINUTE';
+        allData[tickerTf] = [];
+        allData[tickerTf].ohlc = [];
+        allData[tickerTf].timeProcessed = [];
+        allData[tickerTf].snapshot_Long = undefined;
+        allData[tickerTf].snapshot_Short = undefined;
+        allData[tickerTf].inLong = false;
+        allData[tickerTf].inShort = false;
+        allData[tickerTf].entryPrice_Long = 0;
+        allData[tickerTf].entryPrice_Short = 0;
+        allData[tickerTf].initialStopLoss_Long = 0;
+        allData[tickerTf].initialStopLoss_Short = 0;
+        allData[tickerTf].updatedStopLoss_Long = 0;
+        allData[tickerTf].updatedStopLoss_Short = 0;
+        allData[tickerTf].takeProfit_Long = 0;
+        allData[tickerTf].takeProfit_Short = 0;
+      }
+    }
+    return allData;
+  }
+
+  /**
+   * Retourne le ticker timeframe.
+   */
+  getTickerTimeframe(string: string) {
+    const str = string.split('.');
+    const tf = str[4].split(':');
+    return this.getTicker(string) + '_' + tf[1];
+  }
+
+  /**
+   * Retourne le ticker.
+   */
+  getTicker(string: string) {
+    const str = string.split('.');
+    return str[2];
+  }
+
+
+  /**
+   * Permet d'arrêter le processus.
+   */
+  stopProcess() {
+    console.log('Process will be stopped !');
+    process.exit();
+  }
+
+
+  /**
+   * Retourne la date avec décalage horaire.
+   */
+  getDate(): any {
+    let date = new Date();
+    const year = date.getFullYear();
+    const month = '0' + (date.getMonth() + 1);
+    const day = '0' + date.getDate();
+    const hours = '0' + date.getHours();
+    const minutes = '0' + date.getMinutes();
+    const second = '0' + date.getSeconds();
+    return day.substr(-2) + '/' + month.substr(-2) + '/' + year + ' ' + hours.substr(-2) + ':' + minutes.substr(-2) + ':' + second.substr(-2);
+  }
+
+
+  /**
+   * Insert chaque trade dans Firebase.
+   */
+  insertTrade(tickerTfData: any, $tickerTf: string, $winTrades: any, $loseTrades: any, $allTrades: any) {
+    try {
+      let $direction: string;
+      let $entryTime: any;
+      let $entryPrice: number;
+      let $stopLoss: number;
+      let $takeProfit: number;
+      let time: number;
+
+      if (tickerTfData.inLong) {
+        $direction = 'Long';
+        $entryTime = tickerTfData.entryTime_Long;
+        $entryPrice = tickerTfData.entryPrice_Long;
+        $stopLoss = tickerTfData.initialStopLoss_Long;
+        $takeProfit = tickerTfData.takeProfit_Long;
+        time = tickerTfData.snapshot_Long.time;
+      } else if (tickerTfData.inShort) {
+        $direction = 'Short';
+        $entryTime = tickerTfData.entryTime_Short;
+        $entryPrice = tickerTfData.entryPrice_Short;
+        $stopLoss = tickerTfData.initialStopLoss_Short;
+        $takeProfit = tickerTfData.takeProfit_Short;
+        time = tickerTfData.snapshot_Short.time;
+      } else {
+        throw new error;
+      }
+
+      firebase.database().ref('/trade').push({
+        direction: $direction,
+        tickerTf: $tickerTf,
+        setupTime: tickerTfData.ohlc[time].date,
+        entryTime: $entryTime,
+        exitTime: this.getDate(),
+        entryPrice: $entryPrice,
+        stopLoss: $stopLoss,
+        takeProfit: $takeProfit,
+        rr: this.getRiskReward($entryPrice, $stopLoss, $takeProfit)
+      });
+
+      const $avgRR = this.round($allTrades.reduce((a, b) => a + b, 0) / $allTrades.length, 2);
+      const $winrate = this.round(($winTrades.length / ($loseTrades.length + $winTrades.length)) * 100, 2) + '%';
+      firebase.database().ref('/results').remove();
+      firebase.database().ref('/results').push({
+        winTrades: $winTrades.length,
+        loseTrades: $loseTrades.length,
+        totalTrades: $winTrades.length + $loseTrades.length,
+        totalRR: this.round($loseTrades.reduce((a, b) => a + b, 0) + $winTrades.reduce((a, b) => a + b, 0), 2),
+        avgRR: $avgRR ? $avgRR : 0,
+        winrate: $winrate ? $winrate : 0
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+
+  /**
+   * Envoie une notification à Télégram.
+   */
+  sendTelegramMsg(telegramBotObject: any, chatId: string, msg: string) {
+    try {
+      telegramBotObject.sendMessage(chatId, msg);
+    } catch (err) {
+      console.log('Something went wrong when trying to send a Telegram notification', err);
+    }
+  }
+
+
+  /**
+   * Supprime le token si existant et refresh la connection toute les 10h.
+   */
+  async login(ig: any) {
+    const tokenContent = await promisify(fs.readFile)('node_modules\\node-ig-api\\tokens.json', 'UTF-8');
+    const tokenJson = JSON.parse(tokenContent);
+
+    if (tokenJson && tokenJson.tokens_exp > 0) {
+      //await ig.logout();
+      //console.log('logout');
+    }
+
+    await ig.login(true);
+    setInterval(async function () {
+      await ig.login(true);
+    }, 1000 * 60 * 60 * 10)
+
+  }
+}
+
+export default new UtilsService();
+
+
