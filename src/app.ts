a95ad0feec3ab8d29cc6d11dd9facb0aea55a78d
GuysmoB <guysmo@hotmail.fr>
2021-08-05 16:25:25 +0200

    save

--- src/app.ts
+++ src/app.ts
@@ -44,6 +44,7 @@ class App extends CandleAbstract {
   constructor(private utils: UtilsService, private stratService: StrategiesService, private config: Config,
     private indicators: IndicatorsService, private apiService: ApiService) {
     super();
+    process.title = 'orderbook-data';
     console.log('App started |', utils.getDate());
     firebase.initializeApp(config.firebaseConfig);
     this.telegramBot = new TelegramBot(config.token, { polling: false });
